4caa33be12ae9ad2e55ee1dd107097d0932f1b62
topstar628 <topstar628@outlook.com>
2025-12-24 14:49:20 -0700

    pm2 configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'qursar_testnet_miner', // Name of the application
+      script: 'neurons/miner_test.py', // Path to your script
+      interpreter: 'python', // Use the Python interpreter
+      args: '--netuid 439 --subtensor.network test --wallet.name test --wallet.hotkey miner-1 --logging.debug', // Arguments to pass
+      instances: 1, // Number of instances to run
+      exec_mode: 'fork', // Can be 'fork' or 'cluster'
+      env: {
+        NODE_ENV: 'production', // Set environment variables
+      },
+      log_file: './logs/qursar_testnet_miner.log', // Log file path
+      out_file: './logs/qursar_testnet_miner_out.log', // Standard output log
+      error_file: './logs/qursar_testnet_miner_err.log', // Error log
+      merge_logs: true, // Merge logs from all instances
+      autorestart: true, // Automatically restart the app if it crashes
+      watch: false, // Set to true to watch for file changes
+    },
+  ],
+};
